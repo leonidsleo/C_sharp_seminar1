7a6b980626f025a3a48a51c284c57dd990c910c1
leonid <leonidsleo@yandex.ru>
2023-04-20 11:12:50 +0300

    разобрали первые примеры

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Семинар1. Знакомство с языками. Семинар 1
 // Проект1: 
+//int numberCity = 10;
+//int number2 = 15;
+//int number3 = 20;
+//string user = "Leonid";
+
+//Console.Write($"Привет, {user}");
+//Console.WriteLine(user);
+
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Привет, - {num + 100}");
