1c55bb852d1b66bdb79f816d2f9d8b84abf4d298
leonid <leonidsleo@yandex.ru>
2023-04-20 21:09:09 +0300

    создал программу для определения последнего числа в трехзначном числе

--- Project4/Program.cs
+++ Project4/Program.cs
@@ -2,3 +2,9 @@
 //трёхзначное число и на выходе показывает 
 //последнюю цифру этого числа.
 //456 -> 6 	782 -> 2 	918 -> 8
+
+Console.WriteLine("Введите трехзначное число - ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Последняя цифра числа {num} = ");
+Console.Write(num % 10);
