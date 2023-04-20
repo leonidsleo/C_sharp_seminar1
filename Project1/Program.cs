7bc8bfaa54e9e4b17af740112464b2af2d961110
leonid <leonidsleo@yandex.ru>
2023-04-20 11:13:25 +0300

    разобрали первые примеры

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -9,5 +9,11 @@
 //Console.WriteLine(user);
 
 Console.Write("Введите число ");
-int num = Convert.ToInt32(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine()); //сделали конвертацию в числа для сложения
 Console.Write($"Привет, - {num + 100}");
+//Будет выводить Привет, - 400 (100+300)
+
+Console.Write("Введите число ");
+string num = Console.ReadLine(); // тип строка
+Console.Write($"Привет, - {num + 100}");
+//Будет выводить Привет, - 100300 (100+300)
