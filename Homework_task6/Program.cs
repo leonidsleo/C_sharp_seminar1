fc3c8dae57d48f8e4a771f12110d0458ecb3c1a4
leonid <leonidsleo@yandex.ru>
2023-04-21 11:23:38 +0300

    промежуточное сохранение результата с переменной int

--- Homework_task6/Program.cs
+++ Homework_task6/Program.cs
@@ -5,3 +5,21 @@
 //-3 -> нет
 //7 -> нет
 
+Console.WriteLine("Определить, является ли число четным!");
+Console.Write("Введите число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = a / 2;
+int c = b % 1;
+
+if (c == 0)
+{
+    Console.WriteLine("Ответ:");
+    Console.WriteLine($"Число {a} - является четным");
+}
+else
+{
+    Console.WriteLine("Ответ:");
+    Console.WriteLine($"Число {a} - не является четным");
+}
+Console.Write("Прграмма завершена");
