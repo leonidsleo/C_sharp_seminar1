4ad9b81240b2a0905c8972cf69aca3125b4b3539
leonid <leonidsleo@yandex.ru>
2023-04-21 12:11:02 +0300

    formatted the program home_task2 (отформатировал программу в home_task2)

--- Homework_task2/Program.cs
+++ Homework_task2/Program.cs
@@ -5,6 +5,7 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
+Console.WriteLine("Определить какое число больше из двух!");
 Console.Write("Введите число a - ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число b - ");
@@ -16,15 +17,22 @@ Console.WriteLine(b);
 
 if (a == b)
 {
-    Console.WriteLine($"A = {a} равно B = {b}");
+    Console.WriteLine();
+    Console.WriteLine("Ответ:");
+    Console.WriteLine($"Число a = {a} равно b = {b};");
 }
 else
     if (a > b)
     {
-        Console.WriteLine($"A = {a} больше B = {b}");
+        Console.WriteLine();
+        Console.WriteLine("Ответ:");
+        Console.WriteLine($"Число a = {a} больше b = {b};");
     }
     else
     {
-        Console.WriteLine($"A = {a} меньше B = {b}");
+        Console.WriteLine();
+        Console.WriteLine("Ответ:");
+        Console.WriteLine($"Число a = {a} меньше b = {b};");
     }
+Console.WriteLine();
 Console.Write("Программа завершена!");
