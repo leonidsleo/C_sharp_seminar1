37b26b5d5f35188c9d9f9a51e6c6d258db4961f2
leonid <leonidsleo@yandex.ru>
2023-04-21 10:30:36 +0300

    compiled a program for 3- numbers and finding more, took into account if 2 are equal (составил программу для 3- чисел и поиска большего, учел если 2 будут равны)

---
+++ Homework_task4/Program.cs
@@ -0,0 +1,51 @@
+﻿// Семинар 1. Домашнее задание. Задача №4.
+//Напишите программу, которая принимает на вход 
+//три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Введите число a - ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b - ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c - ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("a = ");
+Console.WriteLine(a);
+Console.Write("b = ");
+Console.WriteLine(b);
+Console.Write("c = ");
+Console.WriteLine(c);
+
+if (a > b && a > c)
+{
+    Console.WriteLine("ОТВЕТ:");
+    Console.WriteLine($"Максимальное число = {a}");
+}
+if (a < b && b > c)
+{
+    Console.WriteLine("ОТВЕТ:");
+    Console.WriteLine($"Максимальное число = {b}");
+}
+if (c > b && a < c)
+{
+    Console.WriteLine("ОТВЕТ:");
+    Console.WriteLine($"Максимальное число = {c}");
+}
+if (a == b && a > c)
+{
+    Console.WriteLine("ОТВЕТ:");
+    Console.WriteLine($"Максимальное число = {a}");
+}
+if (a == c && a > b)
+{
+    Console.WriteLine("ОТВЕТ:");
+    Console.WriteLine($"Максимальное число = {a}");
+}
+if (c == b && a < c)
+{
+    Console.WriteLine("ОТВЕТ:");
+    Console.WriteLine($"Максимальное число = {c}");
+}
+Console.Write("Программа завершена!");
