569a4d084c1fc512573cbe1d70a4d02b405a2b21
leonid <leonidsleo@yandex.ru>
2023-04-21 11:22:39 +0300

    создал ведку для проверки типа данных double

--- Project4/Program.cs
+++ Project4/Program.cs
@@ -4,7 +4,7 @@
 //456 -> 6 	782 -> 2 	918 -> 8
 
 Console.WriteLine("Введите трехзначное число - ");
-int num = Convert.ToInt32(Console.ReadLine());
+double num = Convert.ToDouble(Console.ReadLine());
 
 Console.Write($"Последняя цифра числа {num} = ");
-Console.Write(num % 10);
+Console.Write(num % 1);
